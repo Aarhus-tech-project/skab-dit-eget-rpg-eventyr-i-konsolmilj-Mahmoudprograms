37746eaac664310318d2e72c3c0304a85d5b5877
Mahmoud Al Hammoud <at112916@edu.aarhustech.dk>
2025-03-03 08:12:23 +0100

    i finished the Enemy class

---
+++ RPGGame/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RPGGame
+{
+    public class Enemy
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
+
+        public Enemy(string name, int health, int attackPower)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+        }
+
+        public void Attack(Character player)
+        {
+            Console.WriteLine($"{Name} attacks {player.Name} for {AttackPower} damage!");
+            player.TakeDamage(AttackPower);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Name} takes {damage} damage. Remaining health: {Health}");
+
+            if (Health <= 0)
+            {
+                Console.WriteLine($"{Name} has been defeated!");
+            }
+        }
+    }
+}
