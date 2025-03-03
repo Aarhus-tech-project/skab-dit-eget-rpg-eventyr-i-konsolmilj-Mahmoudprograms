d7782edc8f340fc8ad9d468d4065a2847a727ecb
Mahmoud Al Hammoud <at112916@edu.aarhustech.dk>
2025-03-03 08:11:10 +0100

    i finished the Charactrer class

---
+++ RPGGame/Warrior.cs
@@ -0,0 +1,32 @@
+﻿namespace RPGGame
+{
+    public class Warrior : Character
+    {
+        public int ShieldDamage { get; private set; }
+        private int TotalBoomerangDamage;
+
+        public Warrior(string name) : base(name, 100, 15, 5) // (Health, Attack, Defense)
+        {
+            ShieldDamage = 10;
+            TotalBoomerangDamage = 0;
+        }
+
+        public void ThrowBoomerangShield(Enemy enemy)
+        {
+            Console.WriteLine($"{Name} throws the shield like a boomerang, dealing {ShieldDamage} damage!");
+            enemy.TakeDamage(ShieldDamage);
+            TotalBoomerangDamage += ShieldDamage;
+
+            if (TotalBoomerangDamage >= 50)
+            {
+                ActivateSuperPower();
+                TotalBoomerangDamage = 0;
+            }
+        }
+
+        private void ActivateSuperPower()
+        {
+            Console.WriteLine($"{Name} has unlocked the Super Power: Double Damage for 3 turns!");
+        }
+    }
+}
