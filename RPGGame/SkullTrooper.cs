92ceb67a76c3a27910fbda76612bcfba75d2c0d2
Mahmoud Al Hammoud <at112916@edu.aarhustech.dk>
2025-03-03 08:11:29 +0100

    i finished the SkullTrooper class

---
+++ RPGGame/SkullTrooper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPGGame
+{
+    public class SkullTrooper : Character
+    {
+        private Random random;
+
+        public SkullTrooper(string name) : base(name, 100, 10, 3) // (Health, Attack, Defense)
+        {
+            random = new Random();
+        }
+
+        public void ThrowSkull(Enemy enemy)
+        {
+            int damage = random.Next(5, 20); // Skull damage varies between 5 and 20
+            Console.WriteLine($"{Name} throws a skull, dealing {damage} damage!");
+            enemy.TakeDamage(damage);
+        }
+
+        public override void Attack(Enemy enemy)
+        {
+            ThrowSkull(enemy);
+        }
+    }
+}
